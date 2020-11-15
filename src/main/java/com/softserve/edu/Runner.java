94cb31bff6d9cca6002fdc168a6dc02ed0488db6
Andrii Chornyi <chorny.andriy@gmail.com>
2020-11-15 21:58:16 +0200

    edited streams for sorted output of sets in Runner

--- src/main/java/com/softserve/edu/Runner.java
+++ src/main/java/com/softserve/edu/Runner.java
@@ -17,19 +17,22 @@ public class Runner {
         NaturalNumber number = new NaturalNumber(
                 ConsoleDialog
                         .getAnswerFromConsole("Input natural number: "));
-
+        out.println("Natural dividers of " + number.getValue() + ":");
         number.getNaturalDividers()
                 .stream()
                 .sorted()
                 .map(x -> x + " ")
                 .forEach(System.out::print);
 
+        out.println("\nSimple natural dividers of " + number.getValue() + ":");
         number.getSimpleNaturalDividers()
                 .stream()
                 .sorted()
                 .map(x -> x + " ")
                 .forEach(System.out::print);
 
+        out.println("\nNumbers [1.." + number.getValue() + "], whose record coincides with the last " +
+                        "digits of their power 2 :");
         out.println(new TreeMap(number.getPowered2TaleDigitsCoinciders()));
     }
 }
