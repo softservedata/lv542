10408adb9371085b67c5bd14a4e9b116010c3dac
rostyk <rostyk.niko3000@gmail.com>
2020-11-18 19:41:15 +0200

    Format the code

--- src/main/java/com/softserve/edu/App.java
+++ src/main/java/com/softserve/edu/App.java
@@ -4,10 +4,9 @@ package com.softserve.edu;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+	
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
