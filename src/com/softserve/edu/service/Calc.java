0ff8d8bfb4d9e01ada3508dc57197df45e749994
yaroslav <garasym@gmail.com>
2020-11-30 15:04:44 +0200

    List. Examples

---
+++ src/com/softserve/edu/service/Calc.java
@@ -0,0 +1,8 @@
+package com.softserve.edu.service;
+
+public class Calc {
+
+    public double add(double arg0, double arg1) {
+        return arg0 + arg1;
+    }
+}
--- src/main/java/com/softserve/edu/App.java
+++ src/main/java/com/softserve/edu/App.java
@@ -1,13 +1,24 @@
 package com.softserve.edu;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        // String[] a = new String[] { "aa", "bb", "cc", "dd", "aa" };
+        String[] a = { "aa", "bb", "cc", "dd", "aa" };
+        // System.out.println("a = " + Arrays.toString(a));
+        List<String> aList = Arrays.asList(a);
+        System.out.println("aList = " + aList);
+        aList.forEach(s -> System.out.print(s.replaceAll("\\w+", "1111") + "  "));
+        // aList.add("qq"); // Error
+        List<String> sList = new ArrayList<>(aList);
+        sList.add("qq");
+        System.out.println("\nsList = " + sList);
     }
 }
