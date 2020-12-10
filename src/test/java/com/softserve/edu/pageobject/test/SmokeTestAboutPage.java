ace0c3172641fa52718e29f77faf1e193627bbc6
Diana <dianaganushkevych@gmail.com>
2020-12-10 17:20:26 +0200

    Must be finished

---
+++ src/test/java/com/softserve/edu/pageobject/test/SmokeTestAboutPage.java
@@ -0,0 +1,89 @@
+package com.softserve.edu.pageobject.test;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.softserve.edu.pageobject.data.User;
+import com.softserve.edu.pageobject.data.UserRepository;
+import com.softserve.edu.pageobject.pages.about.AboutPage;
+import com.softserve.edu.pageobject.pages.myhabits.MyHabitsPage;
+import com.softserve.edu.pageobject.pages.welcome.WelcomePage;
+
+public class SmokeTestAboutPage extends GreencityTestRunner {
+
+    //@Test
+    public void checkMenu() {
+        // Precondition
+        //setWindowSize(640, 480);
+        //setWindowSize(480, 640);
+        //
+        // Steps
+        WelcomePage welcomePage = loadApplication()
+            .navigateEconews()
+            .navigateTipsTricks()
+            .navigatePlaces()
+            .navigateAbout()
+            .navigateWelcome();
+        //
+        // Check
+        Assert.assertEquals(welcomePage.getButtonStartAttributeClass(),
+                WelcomePage.HEADER_LEFT_ATTRIBUTE);
+    }
+    
+    @DataProvider//(parallel = true)
+    public Object[][] users() {
+        return new Object[][] {
+            { UserRepository.getDefault() },
+        };
+    }
+    
+
+    @Test
+    public void checkText() {
+        // Precondition
+        //setWindowSize(640, 480);
+        //setWindowSize(480, 640);
+        //
+        // Steps
+    	AboutPage isTextOnThePage = loadApplication()
+    			.navigateAbout();
+    	boolean check = isTextOnThePage.isAboutUsTextDisplayed();
+//    			.isAboutUsTextDisplayed();
+    	
+        Assert.assertTrue(check);
+    }
+    
+    
+    //@Test(dataProvider = "users")
+    public void checkLogin(User user) {
+        MyHabitsPage myHabitsPage = loadApplication()
+                .navigateLogin()
+                .successfulLogin(user);
+        Assert.assertEquals(myHabitsPage.getProfileText(),
+                user.getName());
+        WelcomePage welcomePage = myHabitsPage.gotoLogout();
+        Assert.assertTrue(welcomePage.isSignInAvailable());
+    }
+    
+//    @Test(dataProvider = "users")
+//    public void checkMyHabits(User user) {
+//        // Steps
+//        WelcomePage welcomePage = loadApplication()
+//            .navigateMyHabits(user)
+//            .gotoAddHabits()
+//            .navigateEconews()
+//            .navigateTipsTricks()
+//            .navigatePlaces()
+//            .navigateAbout()
+//            .navigateMyHabits()
+//            .navigateWelcome()
+//            .gotoLogout();
+//        //
+//        // Check
+//        Assert.assertEquals(welcomePage.getButtonStartAttributeClass(),
+//                WelcomePage.HEADER_LEFT_ATTRIBUTE);
+//        Assert.assertTrue(welcomePage.isSignInAvailable());
+//    }
+    
+}
