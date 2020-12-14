d810538686c4afcf695206a5cec8d96b50fb729a
Andrii Chornyi <chorny.andriy@gmail.com>
2020-12-14 15:07:02 +0200

    minor changes

--- src/test/java/com/softserve/edu/pageobject/test/andriyc/MenuLoggedOutUserTest.java
+++ src/test/java/com/softserve/edu/pageobject/test/andriyc/MenuLoggedOutUserTest.java
@@ -8,11 +8,39 @@ import org.testng.annotations.Test;
 
 public class MenuLoggedOutUserTest extends GreencityTestRunner {
     @Test
-    void verifyEcoNewMenuTargetPageTitleCompliance() {
+    void verifyEcoNewsMenuTargetPageTitleCompliance() {
         String actualEcoNewsPageTitle = loadApplication()
                 .navigateEconews()
                 .getPageTitle();
 
         Assert.assertEquals(actualEcoNewsPageTitle, "Eco news");
     }
+
+    @Test
+    void verifyTipsTricksMenuTargetPageTitleCompliance() {
+        String actualTipsTricksPageTitle = loadApplication()
+                .navigateTipsTricks()
+                .getPageTitle();
+
+        Assert.assertEquals(actualTipsTricksPageTitle, "Tips & Tricks");
+    }
+
+    @Test
+    void verifyPlacesMenuTargetPageTitleCompliance() {
+        String actualPlacesPageTitle = loadApplication()
+                .navigatePlaces()
+                .getPageTitle();
+
+        Assert.assertEquals(actualPlacesPageTitle, "Places");
+    }
+
+    @Test
+    void verifyAboutUsMenuTargetPageTitleCompliance() {
+        String actualAboutUsPageTitle = loadApplication()
+                .navigateAbout()
+                .getPageTitle();
+
+        Assert.assertEquals(actualAboutUsPageTitle, "About us");
+    }
+
 }
